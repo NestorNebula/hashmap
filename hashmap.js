8f2cad96142cc643ec08350136eb7640cd8e379b
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-06-25 09:06:15 +0200

    Add a new variable capacity
    The capacity represents the number of buckets
    It will be used for populateBuckets instead of the len argument

--- hashmap.js
+++ hashmap.js
@@ -3,14 +3,16 @@ import { LinkedList } from './linkedlist.js';
 export function HashMap() {
   const buckets = [];
 
+  let capacity = 16;
+
   const checkIndex = (index) => {
     if (index < 0 || index >= buckets.length) {
       throw new Error('Trying to access index out of bound');
     }
   };
 
-  const populateBuckets = (len) => {
-    for (let i = 0; i < len; i++) {
+  const populateBuckets = () => {
+    for (let i = 0; i < capacity; i++) {
       buckets[i] = LinkedList();
     }
   };
